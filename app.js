e1f58298b296a0fa544fd6c7d071e1295d0e96f6
Ashay Kanekar <ashaykanekar@gmail.com>
2021-09-30 21:20:31 +0530

    add Procfile

--- app.js
+++ app.js
@@ -91,6 +91,13 @@ app.post("/", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("I am port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+
+// app.listen(3000, function () {
+//   console.log("I am port 3000");
+// });
